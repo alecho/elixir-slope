a8fb7f6e0552c5a0da9d8af6be184af4ccd545e3
Andrew Lechowicz <andrew@alecho.com>
2016-09-05 19:52:44 -0400

    Pattern match list items

--- lib/slope.ex
+++ lib/slope.ex
@@ -1,11 +1,7 @@
 defmodule Slope do
 
-  def calc(cords) do
-    x1 = Enum.at(cords, 0)
-    y1 = Enum.at(cords, 1)
-    x2 = Enum.at(cords, 2)
-    y2 = Enum.at(cords, 3)
-
+  def calc([x1, y1, x2, y2]) do
+    y2
   end
 
 end
