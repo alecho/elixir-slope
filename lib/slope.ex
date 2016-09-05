3bfb831837e015c33d8cfbf2a2512b2f83924a23
Andrew Lechowicz <andrew@alecho.com>
2016-09-05 19:50:45 -0400

    Define Slope.calc/1

--- lib/slope.ex
+++ lib/slope.ex
@@ -1,6 +1,10 @@
 defmodule Slope do
 
-  def calc() do
+  def calc(cords) do
+    x1 = Enum.at(cords, 0)
+    y1 = Enum.at(cords, 1)
+    x2 = Enum.at(cords, 2)
+    y2 = Enum.at(cords, 3)
 
   end
 
