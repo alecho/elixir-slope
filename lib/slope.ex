a40fe5975afdaa0fd088c3c23037e3f346d5b17a
Andrew Lechowicz <andrew@alecho.com>
2016-09-05 21:21:01 -0400

    Add if/else for undefined slope

--- lib/slope.ex
+++ lib/slope.ex
@@ -1,7 +1,11 @@
 defmodule Slope do
 
   def calc([x1, y1, x2, y2]) do
-    div(y2 - y1, x2 - x1)
+    if x2 - x1 == 0 do
+      :undefined
+    else
+      div(y2 - y1, x2 - x1)
+    end
   end
 
 end
