8b344ec2df338c1f322b6c82102aa62b9ecdb1f1
Andrew Lechowicz <andrew@alecho.com>
2016-09-05 21:40:29 -0400

    Pattern match with matching args

--- lib/slope.ex
+++ lib/slope.ex
@@ -1,6 +1,6 @@
 defmodule Slope do
 
-  def calc([x1, _, x2, _]) when x1 == x2, do: :undefined
+  def calc([x, _, x, _]), do: :undefined
 
   def calc([x1, y1, x2, y2]) do
     div(y2 - y1, x2 - x1)
