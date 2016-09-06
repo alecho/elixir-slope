c10c28264a4a8e0beecd17c99c5855dea7f4fe90
Andrew Lechowicz <andrew@alecho.com>
2016-09-05 21:24:25 -0400

    Take advantage elixir's regular syntax keyword lists

--- lib/slope.ex
+++ lib/slope.ex
@@ -1,11 +1,7 @@
 defmodule Slope do
 
   def calc([x1, y1, x2, y2]) do
-    if x2 - x1 == 0 do
-      :undefined
-    else
-      div(y2 - y1, x2 - x1)
-    end
+    if x2 - x1 == 0, do: :undefined, else: div(y2 - y1, x2 - x1)
   end
 
 end
